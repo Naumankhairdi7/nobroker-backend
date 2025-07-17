b228527256bb01e3d3c06582e4d0f1abd73d6472
Nauman Khairdi <142430231+Naumankhairdi7@users.noreply.github.com>
2025-07-17 10:54:19 +0530

    Delete properties.js

--- properties.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addProperty, getProperties } = require('../controllers/propertyController');
-
-router.post('/', addProperty); // Add new property
-router.get('/', getProperties); // Get all properties (with filters)
-
-module.exports = router;
