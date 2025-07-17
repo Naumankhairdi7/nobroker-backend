214a76778cc2a27b54e5a9243bdd3c6a37769d33
Nauman Khairdi <142430231+Naumankhairdi7@users.noreply.github.com>
2025-07-17 10:57:40 +0530

    Delete contact.js

--- contact.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { contact } = require('../controllers/contactController');
-
-router.post('/', contact);
-
-module.exports = router;
